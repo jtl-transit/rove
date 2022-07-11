503230cffa1178f4f2ad9ead28c956cfc6683bdc
yuzhuh <yuzhuh@Mit.edu>
2022-07-11 16:32:06 -0400

    Change docstring abs path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@
 # -- Project information
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../backend/'))
+sys.path.insert(0, os.path.abspath('../../rove/'))
 
 project = 'Rove'
 copyright = '2022, MIT Transit Lab'
